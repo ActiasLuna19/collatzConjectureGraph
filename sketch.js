946cdc30aad484eadac9d0786f0dd9407b6ed89f
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-06 15:07:36 -0400

    Update sketch.js
    -Made the y-axis dependent on the highest term value

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let term = [num]; //What term of the conjecture we are on, and the value of that
 
 let dotSize = 10;
 let xAxisMultipler = 40; //The multipler used in the x axis
-let yAxisMultipler = 10;
+let yAxisMultipler = 10; //The multipler used in the y axis
 
 //calculating the collatz conjecture for the given number
 while (term.length <= failsafe && calcNum != 1) {
@@ -17,15 +17,17 @@ while (term.length <= failsafe && calcNum != 1) {
   }
   
   else if (calcNum %2 != 0 && calcNum != 1){
-    calcNum = (calcNum* 3) +1;
+    calcNum = (calcNum * 3) +1;
     term.push(calcNum);
   }
 }
 
+//variables in relation to the term
+let termMax = Math.max(...term);
 
 //setting up the canvas/ background
 function setup() {
-  createCanvas((term.length * xAxisMultipler) + 175, 700);
+  createCanvas((term.length * xAxisMultipler) + 175, 200 + (termMax * yAxisMultipler));
 }
 
 function draw() {
@@ -35,16 +37,16 @@ function draw() {
   text('The Collatz Conjecture of ' + num, 100, 40);
   
   //Base graph (General layout)
-  line(75, 100, 75, 625); //top line
-  line(75, 625, (term.length * xAxisMultipler) + 75, 625); //bottom line
+  line(75, 100, 75, (termMax * yAxisMultipler) + 100); //top line
+  line(75, (termMax * yAxisMultipler) + 100, (term.length * xAxisMultipler) + 75, (termMax * yAxisMultipler) + 100); //bottom line
   
   
   for(let i = 0; i <= term.length; i++) {
     //the line connecting the dots
-    line(75 + (i * xAxisMultipler), 625 - (term[i] * yAxisMultipler), 75 + (( i+ 1) * xAxisMultipler), 625 - (term[i + 1] * yAxisMultipler));
+    line(75 + (i * xAxisMultipler), (termMax * yAxisMultipler) + 100 - (term[i] * yAxisMultipler), 75 + (( i+ 1) * xAxisMultipler), (termMax * yAxisMultipler) + 100 - (term[i + 1] * yAxisMultipler));
     
     //The dot, term in relation to term value
-    ellipse(75 + (i * xAxisMultipler), 625 - (term[i] * yAxisMultipler), dotSize);
+    ellipse(75 + (i * xAxisMultipler), (termMax * yAxisMultipler) + 100 - (term[i] * yAxisMultipler), dotSize);
   }
   
 }
