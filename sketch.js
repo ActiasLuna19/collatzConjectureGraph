d7ae101c99cb43690beb119629e2b46ea8b4708b
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-07 12:56:15 -0400

    Update sketch.js
    -Cleaned up the code for the graph, made it more variable based so its easier to navigate

--- sketch.js
+++ sketch.js
@@ -1,14 +1,8 @@
-//variable declariation
+//variable declariation for calculations and (some) graph drawing
 let num = 25; //The number given by the user
 let calcNum = num; //The number used in conjecture, the first term is the same as the given number
-const failsafe = 100; //the failsafe so the hardrive wont fry
+const failsafe = 200; //the failsafe so the hardrive wont fry
 let term = [num]; //What term of the conjecture we are on, and the value of that term. Starts with the given number as term 0
-
-let dotSize = 14;
-let xAxisMultipler = 40; //The multipler used in the x axis as the scale
-let yAxisMultipler = 10; //The multipler used in the y axis as the scale
-let graphWidth = 960; //The graph's base width
-let graphLength = 675; //The graph's base length
 
 //calculating the collatz conjecture for the given number
 while (term.length <= failsafe && calcNum != 1) {
@@ -24,8 +18,18 @@ while (term.length <= failsafe && calcNum != 1) {
   }
 }
 
-//variables in relation to the term
+//variables in relation to the term, graph adjustments, and info bar
 let termMax = Math.max(...term); //Finds the highest term value
+let dotSize = 14;
+let xAxisMultipler = 40; //The multipler used in the x axis as the scale
+let yAxisMultipler = 10; //The multipler used in the y axis as the scale
+
+let graphWidth = 960; //The graph's base width
+let graphLength = 675; //The graph's base length
+let aGraphX = 75; //alignment numbers for the graph on the x-axis
+let aGraphY = 100; //alignment number for the graph on the y-axis
+let graphWidthAdd = graphWidth + aGraphX; //The additional numbers needed to allign the graph properly on the x-axis
+let graphLengthAdd = graphLength + aGraphY; //The additional numbers needed to allign the graph properly on the y-axis
 
 //changing the multipler(s) depending on how many terms or the highest term value
 while((termMax * yAxisMultipler) > graphLength) {
@@ -44,31 +48,35 @@ function draw() {
   background(207, 255, 217);
   
   //The Header or Tittle
-  textSize(25);
+  textSize(40);
   fill(0);
-  text('The Collatz Conjecture of ' + num, graphWidth/3, 58);
+  text('The Collatz Conjecture of ' + num, graphWidth - 700, aGraphX - 10);
   
   //The graph background
   fill(255);
-  rect(75, 100, graphWidth, graphLength);
+  rect(aGraphX, aGraphY, graphWidth, graphLength);
   
   //The axes
   strokeWeight(5);
   fill(0);
-  line(75, 100, 75, graphLength + 100); // y-axis
-  line(75, graphLength + 100, graphWidth + 75, graphLength + 100); //x-axis
+  line(aGraphX, aGraphY, aGraphX, graphLengthAdd); // y-axis
+  line(aGraphX, graphLengthAdd, graphWidthAdd, graphLengthAdd); //x-axis
   
   for(let i = 0; i <= term.length; i++) {
     
     //the line connecting the dots
     strokeWeight(2);
-    line(75 + (i * xAxisMultipler),  775 - (term[i] * yAxisMultipler), 75 + (( i+ 1) * xAxisMultipler), 775 - (term[i + 1] * yAxisMultipler));
+    line(aGraphX + (i * xAxisMultipler),  graphLengthAdd - (term[i] * yAxisMultipler), aGraphX + (( i+ 1) * xAxisMultipler), graphLengthAdd - (term[i + 1] * yAxisMultipler));
     
     //The dot, term in relation to term value
     strokeWeight(1);
     fill(242, 188, 70);
-    ellipse(75 + (i * xAxisMultipler), 775 - (term[i] * yAxisMultipler), dotSize);
+    ellipse(aGraphX + (i * xAxisMultipler), graphLengthAdd - (term[i] * yAxisMultipler), dotSize);
   }
+  
+  //The bar on the right, with information
+  fill(247, 223, 178);
+  rect(1075, 25, 350, 750);
   
 }
 
