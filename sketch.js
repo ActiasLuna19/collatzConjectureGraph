b9bbe6d059e8315ae28c82cc0e6558512a16358b
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-06 20:02:31 -0400

    Update sketch.js
    -Made it look a little prettier (visually)

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 //variable declariation
-let num = prompt("Please enter a number here"); //The number given
+let num = 25; //The number given by the user
 let calcNum = num; //The number used in conjecture, the first term is the same as the given number
 const failsafe = 100; //the failsafe so the hardrive wont fry
 let term = [num]; //What term of the conjecture we are on, and the value of that term. Starts with the given number as term 0
 
-let dotSize = 12;
+let dotSize = 14;
 let xAxisMultipler = 40; //The multipler used in the x axis
 let yAxisMultipler = 10; //The multipler used in the y axis
 let screenWidth = 1450;
@@ -36,7 +36,7 @@ while((Math.max(...term) * yAxisMultipler) + 175 >= screenHeight) {
 //variables in relation to the term
 let termMax = Math.max(...term); //Finds the highest term value
 let xAxis = (term.length * xAxisMultipler); //the length added to the x-axis dependat on the term length
-let yAxis = (termMax * yAxisMultipler); //the height added to the y-axis depensant on the highest term
+let yAxis = (termMax * yAxisMultipler + 15); //the height added to the y-axis depensant on the highest term
 
 //setting up the canvas/ background
 function setup() {
@@ -44,21 +44,30 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(207, 255, 217);
   
   textSize(20);
+  fill(0);
   text('The Collatz Conjecture of ' + num, 100, 40);
   
-  //Base graph (General layout)
+  //The graph
+  fill(255);
+  rect(75, 100, xAxis, yAxis);
+  
+  strokeWeight(5);
+  fill(0);
   line(75, 100, 75, yAxis + 100); //top line
   line(75, yAxis + 100, xAxis + 75, yAxis + 100); //bottom line
   
-  
   for(let i = 0; i <= term.length; i++) {
+    
     //the line connecting the dots
+    strokeWeight(2);
     line(75 + (i * xAxisMultipler), yAxis + 100 - (term[i] * yAxisMultipler), 75 + (( i+ 1) * xAxisMultipler), yAxis + 100 - (term[i + 1] * yAxisMultipler));
     
     //The dot, term in relation to term value
+    strokeWeight(1);
+    fill(242, 188, 70);
     ellipse(75 + (i * xAxisMultipler), yAxis + 100 - (term[i] * yAxisMultipler), dotSize);
   }
   
