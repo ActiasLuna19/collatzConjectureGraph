de8bbc61af7ca0fc5f7554d8e63b1d7f2fcfb547
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-18 14:34:59 -0400

    Update sketch.js
    -Moved calculations for the collatz conjecture to a separate file

--- sketch.js
+++ sketch.js
@@ -1,24 +1,4 @@
-//variable declariation for calculations and (some) graph drawing
-let num = 25; //The number given by the user
-let calcNum = num; //The number used in conjecture, the first term is the same as the given number
-const failsafe = 200; //the failsafe so the hardrive wont fry
-let term = [num]; //What term of the conjecture we are on, and the value of that term. Starts with the given number as term 0
-
-//calculating the collatz conjecture for the given number
-while (term.length <= failsafe && calcNum != 1) {
-  
-  if(calcNum %2 == 0) {
-    calcNum = calcNum/2;
-    term.push(calcNum);
-  }
-  
-  else if (calcNum %2 != 0 && calcNum != 1){
-    calcNum = (calcNum * 3) +1;
-    term.push(calcNum);
-  }
-}
-
-//variables in relation to the term, graph adjustments, reset button, and info bar
+//variables decloration for the visuals of the program (The graph, the info-bar, and the "reset" button)
 let termMax = Math.max(...term); //Finds the highest term value
 let xAxisMultipler = 40; //The multipler used in the x axis as the scale
 let yAxisMultipler = 10; //The multipler used in the y axis as the scale
