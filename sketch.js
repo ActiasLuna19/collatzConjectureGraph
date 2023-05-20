49b978bbbfbba7518f15e042dec05e7dfa67df14
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-20 09:13:49 -0400

    Update sketch.js
    -Added the general information to the info-bar
    -Adjusted the list of terms

--- sketch.js
+++ sketch.js
@@ -4,22 +4,27 @@ let xAxisMultipler = 40; //The multipler used in the x axis as the scale
 let yAxisMultipler = 10; //The multipler used in the y axis as the scale
 
 const dotSize = 14; //the size of the dots on the graph
-let graphWidth = 1000; //The graph's base width
-let graphLength = 750; //The graph's base length
+let graphWidth = 1000; //The graph's width
+let graphLength = 750; //The graph's length
 let aGraphX = 50; //alignment numbers for the graph on the x-axis
 let aGraphY = 100; //alignment number for the graph on the y-axis
 let graphWidthAdd = graphWidth + aGraphX; //The additional numbers needed to allign the graph properly on the x-axis
 let graphLengthAdd = graphLength + aGraphY; //The additional numbers needed to allign the graph properly on the y-axis
 
-let barWidth = 375;
-let barLength = graphLength + 75;
-let aBarX = graphWidthAdd + 25;
-let aBarY = aGraphY - 75;
-let aBarListX = aBarX + 10;
-let aBarListY = aBarY + 70;
+let barWidth = 400; //The info-bars width
+let barLength = graphLength + 75; //the info-bars length
+let aBarX = graphWidthAdd + 25; //allignment numbers for the info-bar on the x-axis
+let aBarY = aGraphY - 75; //allignment numbers for the info-bar on the y-axis
+let aBarTextX = aBarX + 10; //the allignment number used to allign the text in the x-axis in the infobar
+let aBarGenY = aBarY + 60;
+let aBarListY = aBarY + 300;
+
+const infoBarHist = 'The Collatz Conjecture or the 3n+1 problem is a famous \nunsolved math problem, created and named after \nLothar Collatz who first introduced the idea in 1937.'; //the history of the collatz conjecture
+const infoBarGen = 'The conjecture states that if an integer or term is even it \nis divided by two, and if it is odd then you multiply the \nterm by three and add one. Eventually, according to the \nconjecture, every sequence of terms will eventually \nbecome 1 no matter what positive term is chosen to start.'; //General information about the collatz conjecture
+const infoBarTerms = 'The terms and their related term value for the number \n' + num + ' are listed below:';
 
 let termListVer; // multiplier used in listing the terms and their value verticaly
-let termListHorz = [0, 75, 150, 225]; //adjuster values needed to list the terms horzontaly
+let termListHorz = [0, 80, 160, 240]; //adjuster values needed to list the terms horzontaly
 let k;
 
 let resButWidth = 200;
@@ -31,11 +36,11 @@ let aButY = barLength - 40; //alignment numbers for the button on the y-axis
 //changing the multipler(s) depending on how many terms or the highest term value
 
 while((termMax * yAxisMultipler) > graphLength) {
-  yAxisMultipler -= 0.1;
+  yAxisMultipler -= 0.01;
 }
 
 while((term.length * xAxisMultipler) > graphWidth) {
-  xAxisMultipler -= 0.1;
+  xAxisMultipler -= 0.01;
 }
 
 //The parameters for the button, used to let the program know when the mouse is over the designated button
@@ -90,14 +95,18 @@ function draw() {
   textSize(25);
   text("Info-bar", aBarX + barWidth/2.7, aBarY + 35);
   
+  //General infomation about the collatz conjecture 
+  textSize(15);
+  text(infoBarHist + '\n\n' + infoBarGen + '\n\n' + infoBarTerms, aBarTextX, aBarGenY);
+  
   //Listing the terms and there value for the number being tested in the collatz conjecture
   k = 0;
   termListVer = 0;
   textSize(15);
   for(let i= 0; i < term.length; i++) {  
     
-    if(termListVer < 30) {
-      text((i + 1) + '. ' + term[i], aBarListX + termListHorz[k], aBarListY + (termListVer * 20));
+    if(termListVer < 23) {
+      text((i + 1) + '. ' + term[i], aBarTextX + termListHorz[k], aBarListY + (termListVer * 20));
       termListVer++
     }
     else {
@@ -105,6 +114,7 @@ function draw() {
       termListVer = 0;
     }
   }
+  
   //the "reset" button visuals on the bottom
   if(buttonParameters()) {
     fill(171, 227, 86);
@@ -131,4 +141,3 @@ function mouseClicked () {
 console.log(xAxisMultipler)
 console.log(yAxisMultipler)
 console.log(term)
-
