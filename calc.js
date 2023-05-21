08fff33df549a4be5f003814704181c5f0d2fc4b
ActiasLuna19 <132719822+ActiasLuna19@users.noreply.github.com>
2023-05-21 17:38:51 -0400

    Update calc.js
    -Fixed a bug involving the failsafe (calculations now stop when the failsafe is hit)
    -Got rid of a redundant line of code

--- calc.js
+++ calc.js
@@ -1,12 +1,12 @@
 //variable declariation for calculations and (some) graph drawing
-let num = prompt('Please write the number you wish to test'); //The number given by the user
+let num = 39;
+//let num = prompt('Please write the number you wish to test'); //The number given by the user
 let calcNum = num; //The number used in conjecture, the first term is the same as the given number
-const failsafe = 200; //the failsafe so the hardrive wont fry
+const failsafe = 115; //the failsafe so the hardrive wont fry
 let term = [num]; //What term of the conjecture we are on, and the value of that term. Starts with the given number as term 0
 
 //calculating the collatz conjecture for the given number
-while (term.length <= failsafe && calcNum != 1) {
-  
+for(let i=1; i < failsafe; i++) {
   if(calcNum %2 == 0) {
     calcNum = calcNum/2;
     term.push(calcNum);
@@ -17,5 +17,3 @@ while (term.length <= failsafe && calcNum != 1) {
     term.push(calcNum);
   }
 }
-
-
